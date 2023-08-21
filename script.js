9fb99200f51193f691d305828c45fc4526e86056
David Islam <david.islam24@gmail.com>
2023-08-21 16:45:00 -0700

    Write async function that hits the API and processes JSON data

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+
+async function getWeatherData(location) {
+  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=6b3cc77dc2ce411a96604927231708&q=${location}`, {
+    mode: 'cors'
+  });
+  const weatherData = await response.json();
+  return {
+    icon: weatherData.current.condition.icon,
+    text: weatherData.current.condition.text,
+    temp_c: weatherData.current.temp_c,
+    temp_f: weatherData.current.temp_f,
+  };
+}
+
+getWeatherData('Vancouver')
+  .then((obj) => {
+    console.log(obj);
+  })
